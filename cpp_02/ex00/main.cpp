a2ead46b9576d5e486e8ed2dd5c8b9b6eb50f9c7
yes-slim <younes.slimani70@gmail.com>
2024-10-07 00:29:00 +0100

    details

--- cpp_02/ex00/main.cpp
+++ cpp_02/ex00/main.cpp
@@ -6,20 +6,23 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 23:56:10 by yes-slim          #+#    #+#             */
-/*   Updated: 2024/10/07 00:11:27 by yes-slim         ###   ########.fr       */
+/*   Updated: 2024/10/07 00:27:56 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main()
-{
+int main() {
+	
 	Fixed	a;
 	Fixed	b(a);
 	Fixed	c;
+	
 	c = b;
+	
 	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+	
 	return 0;
 }
--- cpp_02/ex01/main.cpp
+++ cpp_02/ex01/main.cpp
@@ -6,24 +6,30 @@
 /*   By: yes-slim <yes-slim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/06 23:58:28 by yes-slim          #+#    #+#             */
-/*   Updated: 2024/10/07 00:25:21 by yes-slim         ###   ########.fr       */
+/*   Updated: 2024/10/07 00:28:09 by yes-slim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main() {
-Fixed a;
-Fixed const b( 10 );
-Fixed const c( -42.57f );
-Fixed const d( b );
-a = Fixed( 0.4321f );
-std::cout << "a is " << a << std::endl;
-std::cout << "b is " << b << std::endl;
-std::cout << "c is " << c << std::endl;
-std::cout << "d is " << d << std::endl;
-std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+int	main() {
+	
+	Fixed			a;
+	Fixed	const	b( 10 );
+	Fixed	const c( 42.57f );
+	Fixed	const d( b );
+	
+	a = Fixed( 1234.4321f );
+	
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return 0;
 }
