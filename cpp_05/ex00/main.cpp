d7e1314e2f980b4b8c52c8f1331090186fd6e84f
yes-slim <younes.slimani70@gmail.com>
2025-04-21 05:30:05 +0100

    05/02

--- cpp_05/ex00/main.cpp
+++ cpp_05/ex00/main.cpp
@@ -5,9 +5,9 @@ int main(void)
 
     try
     {
-        Bureaucrat A("unkown",151);
-        // for (size_t i = 0; i < 15; i++)
-        //     A.increment();
+        Bureaucrat A("unkown",1);
+        for (size_t i = 0; i < 15; i++)
+            A.increment();
         std::cout << A << std::endl;
     }
     catch(const std::exception& e)
